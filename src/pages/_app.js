8af925b6fbcbd06ab0549caad7a909a473cbc056
teemago <65630024+teemago@users.noreply.github.com>
2024-03-25 18:29:40 -0300

    0.1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
+
+import Head from "next/head";
+import { Fragment } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="Portfolio de Arthur Nonaka Oda" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
